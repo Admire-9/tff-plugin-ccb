b2d53bafab5d3b3a8c9af8573b680271e800f841
admire <admire.huo@toursforfun.cn>
2018-08-03 15:27:44 +0800

    tff ccb java change

--- www/tffccb.js
+++ www/tffccb.js
@@ -2,6 +2,6 @@ var exec = require('cordova/exec');
 
 module.exports = {
     pay : function(params, success, error) {
-        exec(success, error, "TFFCCB", "pay", [params]);
+        exec(success, error, "TffCCB", "pay", [params]);
     }
 };
